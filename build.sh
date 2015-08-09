2ff7201346bdde199f9b1de05838fd6863da1ce5
Chris Ward <chriswhward@gmail.com>
2015-08-09 14:24:03 +0200

    script had wrong folder location

--- build.sh
+++ build.sh
@@ -16,4 +16,4 @@ for filename in _cards/*.md; do
   pandoc --from=markdown+yaml_metadata_block --template _layouts/cards.latex -o pod/pdf/cards/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 done
 
-pdfjam _site/pdf/cards/*.pdf --no-landscape --frame true --nup 3x3 --delta "0.2cm 0.2cm" --noautoscale=true --outfile pod/cards_complete.pdf
+pdfjam pod/pdf/cards/*.pdf --no-landscape --frame true --nup 3x3 --delta "0.2cm 0.2cm" --noautoscale=true --outfile pod/cards_complete.pdf
