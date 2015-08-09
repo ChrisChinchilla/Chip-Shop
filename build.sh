25d3a969cae81259da31f56fa874240156cef5bb
Chris Ward <chriswhward@gmail.com>
2015-08-09 14:18:07 +0200

    Removed multicard latex file and added new build command for 9up pdf

--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@ bundle install
 bundle update
 
 rm -dfr _site
+rm -dfr pod
 
 jekyll build
 
-# TODO: Move this out of site root
-mkdir -p _site/pdf/cards
+mkdir -p pod/pdf/cards
 
 for filename in _cards/*.md; do
   echo $filename
-  pandoc --from=markdown+yaml_metadata_block --template _layouts/cards.latex -o _site/pdf/cards/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+
+  pandoc --from=markdown+yaml_metadata_block --template _layouts/cards.latex -o pod/pdf/cards/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 done
+
+pdfjam _site/pdf/cards/*.pdf --no-landscape --frame true --nup 3x3 --delta "0.2cm 0.2cm" --noautoscale=true --outfile pod/cards_complete.pdf
