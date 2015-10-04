d879f8577b30c6e634900123dfe304cdcd770f31
Chris Ward <chriswhward@gmail.com>
2015-10-04 13:06:07 +0100

    Beginning of manual latex file and build process

--- build.sh
+++ build.sh
@@ -8,14 +8,30 @@ rm -dfr pod
 
 jekyll build
 
-mkdir -p pod/pdf/cards
+# Process Cards
+# mkdir -p pod/pdf/cards
+#
+# echo "Processing Game Cards…"
+# for filename in _cards/*.md; do
+#   echo $filename
+#
+#   pandoc --from=markdown+yaml_metadata_block --template _layouts/cards.latex -o pod/pdf/cards/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+# done
+#
+# pdfjam pod/pdf/cards/*.pdf --no-landscape --frame true --nup 3x3 --suffix complete --outfile ./cards.pdf
+#
+# mv ./cards.pdf pod/cards_complete.pdf
+#
+# # Process Manual
+# mkdir -p pod/pdf/manual
 
-for filename in _cards/*.md; do
+echo "Processing Manual…"
+for filename in _manual/*.md; do
   echo $filename
 
-  pandoc --from=markdown+yaml_metadata_block --template _layouts/cards.latex -o pod/pdf/cards/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+  pandoc --from=markdown+yaml_metadata_block --template _layouts/manual.latex -o pod/pdf/manual/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 done
 
-pdfjam pod/pdf/cards/*.pdf --no-landscape --frame true --nup 3x3 --suffix complete --outfile ./cards.pdf
+pdfjam pod/pdf/manual/*.pdf --no-landscape --suffix complete --outfile ./manual.pdf
 
-mv ./cards.pdf pod/cards_complete.pdf
+mv ./manual.pdf pod/manual_complete.pdf
