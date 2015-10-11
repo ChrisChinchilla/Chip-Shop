e2045b5dabd3c8b418a7a542cd4a54a5b6c7552c
Chris Ward <chriswhward@gmail.com>
2015-10-11 19:29:18 +0200

    Seperating legal notices and some minor tweaks to card layout

--- build.sh
+++ build.sh
@@ -10,18 +10,26 @@ jekyll build
 
 # Process Cards
 mkdir -p pod/pdf/cards
+mkdir -p pod/pdf/legal
+
 
 echo "Processing Game Cards…"
 for filename in _cards/*.md; do
   echo $filename
 
   pandoc --from=markdown+yaml_metadata_block --template _layouts/cards.latex -o pod/pdf/cards/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+
+  pandoc --from=markdown+yaml_metadata_block --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+
 done
 
 pdfjam pod/pdf/cards/*.pdf --no-landscape --frame true --nup 3x3 --suffix complete --outfile ./cards.pdf
 
 mv ./cards.pdf pod/cards_complete.pdf
 
+pdfjam pod/pdf/legal/*.pdf --no-landscape --nup 1x1 --suffix complete --outfile ./legal_notices.pdf
+
+mv ./legal_notices.pdf pod/legal_notices.pdf
 
 # Process Manual
 mkdir -p pod/pdf/manual
