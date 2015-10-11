a5e6889910eaf3c386c60f841cff803b066cfbd1
Chris Ward <chriswhward@gmail.com>
2015-10-11 11:14:12 +0200

    Adding concepts to build file

--- build.sh
+++ build.sh
@@ -29,3 +29,17 @@ mkdir -p pod/pdf/manual
 echo "Processing Manual…"
 
 pandoc --from=markdown+yaml_metadata_block --template _layouts/manual_print.latex -o pod/pdf/manual/manual.pdf --latex-engine=xelatex manual.md
+
+# Process Concepts
+mkdir -p pod/pdf/concepts
+
+echo "Processing Concepts…"
+for filename in _concepts/*.md; do
+  echo $filename
+
+  pandoc --from=markdown+yaml_metadata_block --template _layouts/concepts.latex -o pod/pdf/concepts/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+done
+
+pdfjam pod/pdf/concepts/*.pdf --no-landscape --frame false --nup 5x5 --suffix complete --outfile ./concepts.pdf
+
+mv ./concepts.pdf pod/concepts_complete.pdf
