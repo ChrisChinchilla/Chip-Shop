b0c73ff20245d8b64cdc485050600008bab41c10
Chris Ward <chriswhward@gmail.com>
2016-01-28 13:25:41 +0100

    New build process

--- rebuild.sh
+++ rebuild.sh
@@ -10,12 +10,12 @@ jekyll build
 mkdir -p pod/pdf/cards
 mkdir -p pod/pdf/cards/events
 mkdir -p pod/pdf/cards/personalities
+mkdir -p pod/pdf/cards/products
 mkdir -p pod/pdf/income
 mkdir -p pod/pdf/legal
 mkdir -p simulators/tabletop_simulator/cards/events
 mkdir -p simulators/tabletop_simulator/cards/personalities
-# mkdir -p simulators/tabletop_simulator/cards/events/png
-
+mkdir -p simulators/tabletop_simulator/cards/products
 
 echo "Processing Events Cards…"
 for filename in _cards/events/*.md; do
@@ -75,24 +75,25 @@ montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simul
 # pdfjam pod/pdf/legal/*.pdf --no-landscape --nup 1x1 --suffix complete --outfile ./legal_notices.pdf
 #
 # mv ./legal_notices.pdf pod/legal_notices.pdf
-#
-# # Process Manual
-# mkdir -p pod/pdf/manual
-#
-# echo "Processing Manual…"
-#
-# pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/manual_print.latex -o pod/pdf/manual/manual.pdf --latex-engine=xelatex manual.md
-#
-# # Process Concepts
-# mkdir -p pod/pdf/concepts
-#
-# echo "Processing Concepts…"
-# for filename in _concepts/*.md; do
-#   echo $filename
-#
-#   pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/concepts.latex -o pod/pdf/concepts/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
-# done
-#
-# pdfjam pod/pdf/concepts/*.pdf --no-landscape --frame false --nup 5x5 --suffix complete --outfile ./concepts.pdf
-#
-# mv ./concepts.pdf pod/concepts_complete.pdf
+
+
+# Process Manual
+mkdir -p pod/pdf/manual
+
+echo "Processing Manual…"
+
+pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/manual_print.latex -o pod/pdf/manual/manual.pdf --latex-engine=xelatex manual.md
+
+# Process Concepts
+mkdir -p pod/pdf/concepts
+
+echo "Processing Concepts…"
+for filename in _concepts/*.md; do
+  echo $filename
+
+  pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/concepts.latex -o pod/pdf/concepts/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
+done
+
+pdfjam pod/pdf/concepts/*.pdf --no-landscape --frame false --nup 5x5 --suffix complete --outfile ./concepts.pdf
+
+mv ./concepts.pdf pod/concepts_complete.pdf
