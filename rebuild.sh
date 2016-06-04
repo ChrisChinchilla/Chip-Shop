2696fef1936bb1143291b0b24de5964606ab1735
Chris Ward <chriswhward@gmail.com>
2016-06-04 10:26:57 +0200

    Comment out emulators for now

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@ bundle update
 
 rm -dfr _site
 rm -dfr pod
-rm -dfr simulators
+#rm -dfr simulators
 
 jekyll build
 
@@ -18,14 +18,14 @@ mkdir -p pod/pdf/cards/products/sub_era_4
 mkdir -p pod/pdf/cards/products/sub_era_5
 mkdir -p pod/pdf/income
 mkdir -p pod/pdf/legal
-mkdir -p simulators/tabletop_simulator/cards/events
-mkdir -p simulators/tabletop_simulator/cards/personalities
-mkdir -p simulators/tabletop_simulator/cards/products/accessories
-mkdir -p simulators/tabletop_simulator/cards/products/sub_era_1
-mkdir -p simulators/tabletop_simulator/cards/products/sub_era_2
-mkdir -p simulators/tabletop_simulator/cards/products/sub_era_3
-mkdir -p simulators/tabletop_simulator/cards/products/sub_era_4
-mkdir -p simulators/tabletop_simulator/cards/products/sub_era_5
+# mkdir -p simulators/tabletop_simulator/cards/events
+# mkdir -p simulators/tabletop_simulator/cards/personalities
+# mkdir -p simulators/tabletop_simulator/cards/products/accessories
+# mkdir -p simulators/tabletop_simulator/cards/products/sub_era_1
+# mkdir -p simulators/tabletop_simulator/cards/products/sub_era_2
+# mkdir -p simulators/tabletop_simulator/cards/products/sub_era_3
+# mkdir -p simulators/tabletop_simulator/cards/products/sub_era_4
+# mkdir -p simulators/tabletop_simulator/cards/products/sub_era_5
 
 
 echo "Processing Product Accessory Cards…"
@@ -39,14 +39,14 @@ for filename in _cards/products/accessories/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/accessories/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/accessories/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/accessories/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/accessories/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/products/accessories/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./products_accessories_cards.pdf
 mv ./products_accessories_cards.pdf pod/products_accessories_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/accessories/*.png simulators/tabletop_simulator/cards/products_accessories_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/accessories/*.png simulators/tabletop_simulator/cards/products_accessories_card_sheet.png
 
 echo "Processing Product Sub Era 1 Cards…"
 for filename in _cards/products/sub_era_1/*.md; do
@@ -60,14 +60,14 @@ for filename in _cards/products/sub_era_1/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_1/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_1/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_1/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_1/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/products/sub_era_1/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./products_sub_era_1_cards.pdf
 mv ./products_sub_era_1_cards.pdf pod/products_sub_era_1_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_1/*.png simulators/tabletop_simulator/cards/products_sub_era_1_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_1/*.png simulators/tabletop_simulator/cards/products_sub_era_1_card_sheet.png
 
 echo "Processing Product Sub Era 2 Cards…"
 for filename in _cards/products/sub_era_2/*.md; do
@@ -81,14 +81,14 @@ for filename in _cards/products/sub_era_2/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_2/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_2/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_2/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_2/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/products/sub_era_2/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./products_sub_era_2_cards.pdf
 mv ./products_sub_era_2_cards.pdf pod/products_sub_era_2_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_2/*.png simulators/tabletop_simulator/cards/products_sub_era_2_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_2/*.png simulators/tabletop_simulator/cards/products_sub_era_2_card_sheet.png
 
 echo "Processing Product Sub Era 3 Cards…"
 for filename in _cards/products/sub_era_3/*.md; do
@@ -102,14 +102,14 @@ for filename in _cards/products/sub_era_3/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_3/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_3/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_3/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_3/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/products/sub_era_3/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./products_sub_era_3_cards.pdf
 mv ./products_sub_era_3_cards.pdf pod/products_sub_era_3_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_3/*.png simulators/tabletop_simulator/cards/products_sub_era_3_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_3/*.png simulators/tabletop_simulator/cards/products_sub_era_3_card_sheet.png
 
 echo "Processing Product Sub Era 4 Cards…"
 for filename in _cards/products/sub_era_4/*.md; do
@@ -123,14 +123,14 @@ for filename in _cards/products/sub_era_4/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_4/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_4/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_4/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_4/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/products/sub_era_4/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./products_sub_era_4_cards.pdf
 mv ./products_sub_era_4_cards.pdf pod/products_sub_era_4_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_4/*.png simulators/tabletop_simulator/cards/products_sub_era_4_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_4/*.png simulators/tabletop_simulator/cards/products_sub_era_4_card_sheet.png
 
 echo "Processing Product Sub Era 5 Cards…"
 for filename in _cards/products/sub_era_5/*.md; do
@@ -144,14 +144,14 @@ for filename in _cards/products/sub_era_5/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_5/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_5/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/products/sub_era_5/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/products/sub_era_5/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/products/sub_era_5/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./products_sub_era_5_cards.pdf
 mv ./products_sub_era_5_cards.pdf pod/products_sub_era_5_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_5/*.png simulators/tabletop_simulator/cards/products_sub_era_5_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/products/sub_era_5/*.png simulators/tabletop_simulator/cards/products_sub_era_5_card_sheet.png
 
 cp assets/print/products_card_back.png simulators/tabletop_simulator/cards/products_card_back.png
 
@@ -169,14 +169,14 @@ for filename in _cards/events/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 72 -depth 8 -quality 85 pod/pdf/cards/events/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/events/"$(basename "$filename" .md)".png
+  #convert -density 72 -depth 8 -quality 85 pod/pdf/cards/events/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/events/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/events/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./events_cards.pdf
 mv ./events_cards.pdf pod/events_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/events/*.png simulators/tabletop_simulator/cards/events_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/events/*.png simulators/tabletop_simulator/cards/events_card_sheet.png
 
 cp assets/print/events_card_back.png simulators/tabletop_simulator/cards/events_card_back.png
 
@@ -193,14 +193,14 @@ for filename in _cards/personalities/*.md; do
   # pandoc --from=markdown+yaml_metadata_block --smart --template _layouts/legal.latex -o pod/pdf/legal/"$(basename "$filename" .md)".pdf --latex-engine=xelatex $filename
 
   # Create Individual PNGs
-  convert -density 300 -depth 8 -quality 85 pod/pdf/cards/personalities/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/personalities/"$(basename "$filename" .md)".png
+  #convert -density 300 -depth 8 -quality 85 pod/pdf/cards/personalities/"$(basename "$filename" .md)".pdf simulators/tabletop_simulator/cards/personalities/"$(basename "$filename" .md)".png
 done
 
 pdfjam pod/pdf/cards/personalities/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./personalities_cards.pdf
 mv ./personalities_cards.pdf pod/personalities_cards.pdf
 
 # Create Card Sheets for Tabletop Simulator
-montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/personalities/*.png simulators/tabletop_simulator/cards/personalities_card_sheet.png
+#montage -verbose -define png:size=484x744 -geometry 484x744+2+2 -tile 10x7 simulators/tabletop_simulator/cards/personalities/*.png simulators/tabletop_simulator/cards/personalities_card_sheet.png
 
 cp assets/print/personalities_card_back.png simulators/tabletop_simulator/cards/personalities_card_sheet.png
 
@@ -245,3 +245,11 @@ done
 pdfjam pod/pdf/concepts/*.pdf --no-landscape --frame false --nup 5x5 --suffix complete --outfile ./concepts.pdf
 
 mv ./concepts.pdf pod/concepts_complete.pdf
+
+# TODO: Delete original files?
+
+# Print All together
+
+pdfjam pod/pdf/cards/products/accessories/*.pdf pod/pdf/cards/products/sub_era_1/*.pdf pod/pdf/cards/products/sub_era_2/*.pdf pod/pdf/cards/products/sub_era_3/*.pdf pod/pdf/cards/products/sub_era_4/*.pdf pod/pdf/cards/products/sub_era_5/*.pdf pod/pdf/cards/events/*.pdf pod/pdf/cards/personalities/*.pdf --no-landscape --frame true --nup 5x4 --suffix complete --outfile ./comp_print_cards.pdf
+
+mv ./comp_print_cards.pdf pod/comp_print_cards.pdf
